0db24e6a3f0ddf496be023176e1e689fd163fa2a
Kristiamnx <rivaskristopher24@gmail.com>
2024-10-28 20:32:59 -0500

    Normalizar finales de línea

---
+++ src/main/java/com/TD/BL_Monolith_TD/BlMonolithTdApplication.java
@@ -0,0 +1,17 @@
+package com.TD.BL_Monolith_TD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlMonolithTdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlMonolithTdApplication.class, args);
+		System.out.println("|----------------------------------------------------------------------------|");
+		System.out.println("|----------------------- !!!COMPILACIÓN EXITOSA¡¡¡ --------------------------|");
+		System.out.println("|----------------------------------------------------------------------------|");
+	}
+
+
+}
---
+++ src/test/java/com/TD/BL_Monolith_TD/BlMonolithTdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TD.BL_Monolith_TD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlMonolithTdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
