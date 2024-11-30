7300ffc7522c4a6da58804913b07e527466b928f
Kristianmx <rivaskristopher24@gmail.com>
2024-11-30 06:28:42 +0000

    test

--- src/main/java/com/TD/BL_Monolith_TD/config/RedisConfig.java
+++ src/main/java/com/TD/BL_Monolith_TD/config/RedisConfig.java
@@ -1,16 +1,21 @@
 package com.TD.BL_Monolith_TD.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("172.17.0.1");
+        config.setPort(6379);
+        return new LettuceConnectionFactory(config);
     }
 }
+
