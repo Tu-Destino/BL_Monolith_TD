30c8ecb7092edaa3039fb4966a13f8da08103f8c
Kristiamnx <rivaskristopher24@gmail.com>
2024-11-30 01:49:31 -0500

    config

--- src/main/java/com/TD/BL_Monolith_TD/api/dto/response/PlaceResponse.java
+++ src/main/java/com/TD/BL_Monolith_TD/api/dto/response/PlaceResponse.java
@@ -3,12 +3,17 @@ package com.TD.BL_Monolith_TD.api.dto.response;
 import com.TD.BL_Monolith_TD.util.enums.Enum_Type;
 import lombok.*;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class PlaceResponse {
+public class PlaceResponse implements Serializable {
+    @Serial
+    private static final long serialVersionUID=1L;
     private Long id;
     private Enum_Type enum_type;
     private String title;
--- src/main/java/com/TD/BL_Monolith_TD/api/dto/response/PostDiscoverResponse.java
+++ src/main/java/com/TD/BL_Monolith_TD/api/dto/response/PostDiscoverResponse.java
@@ -2,12 +2,17 @@ package com.TD.BL_Monolith_TD.api.dto.response;
 
 import lombok.*;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class PostDiscoverResponse {
+public class PostDiscoverResponse implements Serializable {
+    @Serial
+    private static final long serialVersionUID=1L;
     private String id;
     private String title;
     private String description;
