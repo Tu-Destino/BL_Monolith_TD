78bc4340cbdcec46b91c1c1fb6b5e3ef261f5a46
Kristianmx <rivaskristopher24@gmail.com>
2024-12-18 17:40:13 +0000

    reconfigure swagger from http to https

--- src/main/java/com/TD/BL_Monolith_TD/config/OpenApiConfiguration.java
+++ src/main/java/com/TD/BL_Monolith_TD/config/OpenApiConfiguration.java
@@ -1,25 +1,42 @@
 package com.TD.BL_Monolith_TD.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(
+        info = @io.swagger.v3.oas.annotations.info.Info(
                 title = "DestinoAPI",
                 version = "3.1",
                 description = "<b>DestinoAPI:</b> The Ultimate Information Hub for <i>Tu Destino</i>.<br>Providing precise information, advanced filtering, and seamless email dispatch services.<br>Tailored for a superior user experience, ensuring all the information you need is just a click away.",
-                contact = @Contact(
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
                         name = "Support",
                         email = "support@tudestino.com",
-                        url = "https://tu-destino-v3-0-wed.vercel.app/"
+                        url = "https://www.tudestino.coalmd.com/"
                 )
         )
-       //, security = @SecurityRequirement(name = "basicAuth")
 )
 public class OpenApiConfiguration {
 
-
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server()
+                        .url("https://tudestinoresourse.coalmd.com/TD/api/v1")
+                        .description("Production Server"))
+                .info(new Info()
+                        .title("DestinoAPI")
+                        .version("3.1")
+                        .description("<b>DestinoAPI:</b> The Ultimate Information Hub for <i>Tu Destino</i>.<br>Providing precise information, advanced filtering, and seamless email dispatch services.<br>Tailored for a superior user experience, ensuring all the information you need is just a click away.")
+                        .contact(new Contact()
+                                .name("Support")
+                                .email("support@tudestino.com")
+                                .url("https://www.tudestino.coalmd.com/")));
+    }
 }
